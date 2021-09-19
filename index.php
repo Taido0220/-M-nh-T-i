73b4751430f4d874703c153da2750b26177f8cf4
Taido0220 <91017652+Taido0220@users.noreply.github.com>
2021-09-19 09:11:07 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thuchanh01HTML</title>
+    <link rel ="stylesheet" href="css/style2.css">
+
+</head> 
+<body>
+
+ <section class="header">
+    <div class ="container">
+         <div class="logo">
+              <img src ="Images/logo.png" alt=""class="img-responsive">
+         </div>
+
+       <div class="Menu text-right">
+          <ul>
+               <li>
+                  <a href="#" >Home</a>  
+               </li> 
+               <li>
+                   <a  href="#" >Catagories</a>
+               </li> 
+               <li>
+                    <a  href="#" >Food</a>
+               </li> 
+               <li>
+                    <a  href="#" >Contact</a>
+               </li> 
+           </ul>
+       </div>
+       <div class="clear"></div>
+   </div>
+   
+
+    
+</section>
+   
+
+
+
+ <section class="Food-Search text-center">
+     <div class="container">
+           <form action="">
+               <input type="search" name="search" placeholder ="Search for food...">
+               <input type="submit" name="submit" value ="Search" class="btn btn-primary">
+           </form>
+       </div>
+ </section>
+  <section class="Categories">
+   <div class="container">
+           <h2 class="text-center"> Explore Food</h2>
+      <div class="box-3 float-container">
+       <img src ="Images/pizza.jpg" alt="Pizza"class="img-responsive img-curve">
+        <h3 class="float-text text-white">Pizza</h3>
+        </div>
+
+      <div class="box-3 float-container ">
+      <img src ="Images/burger.jpg" alt="Burger"class="img-responsive img-curve">
+        <h3 class="float-text text-white">Burger</h3>
+      </div>
+
+      <div class="box-3 float-container">
+      <img src ="Images/momo.jpg" alt="Momo"class="img-responsive img-curve">
+        <h3 class="float-text text-white">Momo</h3>
+     </div>
+     <div class ="clear"></div>
+    </div>
+ </section>
+
+ <section class="Food-menu">
+     <div class="container"> 
+           <h2 class="text-center">Food menu </h2>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-pizza.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Food Title</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-burger.jpg" alt="burger chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Smoky Burger</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-burger.jpg" alt="burger chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Smoky Burger</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-pizza.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Food Title</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-burger.jpg" alt="burger chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Smoky Burger</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+
+           <div class="Food-menu-box">
+               <div class="food-menu-img">
+                 <img src ="Images/menu-momo.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Chicken Steam Momo</h4>
+                  <p class="food-price" >$2.3</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-primary">Order Now</a>
+
+              </div>
+           </div>
+           <div class="clear"></div>
+     </div>
+ </section>
+
+ <section class="social">
+ <div class="container text-center"> 
+ <ul>
+               <li>
+                  <a href="#"><img src="https://img.icons8.com/fluency/48/000000/facebook.png"/></a>  
+               </li> 
+               <li>
+                   <a  href="#" ><img src="https://img.icons8.com/color/48/000000/instagram-new.png"/></a>
+               </li> 
+               <li>
+                    <a  href="#" ><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></a>
+               </li> 
+
+    </ul>
+</div>
+</section>
+
+<section class="end">
+ <div class="container text-center"> 
+ <p>All rights reserved. Designed By <a href="#" >CSE.TLU</a><p>  
+</div>
+</section> 
+
+
+</section>
+</body>
+</html>
